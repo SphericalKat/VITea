c86ac0f07cfb760cbb9195a3bf08036b56bb96a2
ATechnoHazard <amolele@gmail.com>
2020-07-22 02:39:57 +0530

    res: set up layouts and navigation
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- app/src/main/java/com/example/vitea/ui/MainActivity.kt
+++ app/src/main/java/com/example/vitea/ui/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.vitea.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.vitea.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val spannable = SpannableString("VITea")
+        spannable.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(this, R.color.neon_green)),
+            2, 5,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        toolbar_text.text = spannable
+
+        NavigationUI.setupWithNavController(bottomNav, navHostFragment.findNavController())
     }
 }
