054a17b96fcead09079c2cbc1b034c36c1efdb7a
ATechnoHazard <amolele@gmail.com>
2020-07-25 02:04:28 +0530

    fragments: day: optimize imports and layout
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- app/src/main/java/com/example/vitea/ui/TimeTableFragment.kt
+++ app/src/main/java/com/example/vitea/ui/TimeTableFragment.kt
@@ -4,16 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.vitea.R
-import com.example.vitea.adapters.TimeTableAdapter
 import com.example.vitea.models.ApiResult
-import com.example.vitea.models.timetable.Lecture
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_time_table.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
