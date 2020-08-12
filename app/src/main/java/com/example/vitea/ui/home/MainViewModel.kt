e0e3dc342c232f73abbb72c6dec7fa0a1fbb9ea2
ATechnoHazard <amolele@gmail.com>
2020-08-13 00:26:56 +0530

    timetable: show previously fetched data and update in the background
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- app/src/main/java/com/example/vitea/ui/home/MainViewModel.kt
+++ app/src/main/java/com/example/vitea/ui/home/MainViewModel.kt
@@ -1,19 +1,31 @@
 package com.example.vitea.ui.home
 
+import android.content.SharedPreferences
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.vitea.models.ApiResult
 import com.example.vitea.models.timetable.TimeTableResponse
 import com.example.vitea.repository.WebRepo
+import com.google.gson.Gson
+import com.example.vitea.utils.PreferenceHelper.get
+import com.example.vitea.utils.PreferenceHelper.set
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class MainViewModel(private val webRepo: WebRepo): ViewModel() {
+class MainViewModel(private val webRepo: WebRepo, private val prefs: SharedPreferences): ViewModel() {
     val timeTable = MutableLiveData<ApiResult<TimeTableResponse>>(ApiResult.loading())
 
     fun getTimeTable(regNo: String) = viewModelScope.launch(Dispatchers.IO) {
+        val gson = Gson()
+        if (prefs["saved_timetable", ""] != "") {
+            val saved = gson.fromJson(prefs["saved_timetable", ""], TimeTableResponse::class.java)
+            timeTable.postValue(ApiResult.success(saved))
+        }
         val result = webRepo.getTimeTableAsync(regNo)
+        result.data?.let {
+            prefs["saved_timetable"] = gson.toJson(result.data)
+        }
         timeTable.postValue(result)
     }
 }
