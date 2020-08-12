40e041932639f8520690cb4a4322d6d6606b5f9d
ATechnoHazard <amolele@gmail.com>
2020-08-13 00:37:17 +0530

    timetable: scroll to the current day by default
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- app/src/main/java/com/example/vitea/ui/home/TimeTableFragment.kt
+++ app/src/main/java/com/example/vitea/ui/home/TimeTableFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -15,6 +16,8 @@ import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_time_table.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.sharedViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TimeTableFragment : Fragment() {
 
@@ -38,12 +41,19 @@ class TimeTableFragment : Fragment() {
         viewModel.timeTable.observe(viewLifecycleOwner, Observer {
             when (it.status) {
                 ApiResult.Status.LOADING -> {
-                    viewModel.getTimeTable(prefs["reg_no", "17BEE0001"]!!)
+                    viewModel.getTimeTable(prefs["reg_no", "17BEE0001"].toString())
                 }
 
                 else -> {}
             }
         })
+
+        pager.post {
+            val sdf = SimpleDateFormat("EEEE")
+            val d = Date()
+            val dayOfTheWeek: String = sdf.format(d)
+            pager.setCurrentItem(dayList.indexOf(dayOfTheWeek), true)
+        }
     }
 
     class DayAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
